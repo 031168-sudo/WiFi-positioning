c27d5dfa6f66c3fe0488f42cc15a17ea283e2d4e
031168-sudo <031168@gmail.com>
2026-09-03 16:25:54 +0300

    Fix Gradle repository configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 rootProject.name = "WiFiPositioning"
 include(":app")
